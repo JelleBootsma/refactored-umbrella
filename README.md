5b2f1720b78baddb210dbc1be3fd61a1d8d50a49
Jelle Bootsma <j.h.bootsma@student.tue.nl>
2019-07-24 11:25:05 +0200

    Added hidden subtext to menu's, and cleaned up the structure

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # refactored-umbrella
 Test site I can use to test stuff and use to fork when i need to create a website for some reason
-Uses HTML, CSS and JQuery, so a bit outdated, but well tested
+Uses HTML, CSS and JQuery, so a bit outdated, but well tested. The PHP is just there to be able to host it on Heroku
+
+The site is for now filled with open stock images, but these are just placeholders.
--- app.js
+++ app.js
@@ -27,43 +27,51 @@ var main = function() {
   });
   $('#menu1').mouseenter(function() {
 	$('#menu1').animate({
-		height: "70px"
+		height: "50px"
 	}, 200);
+	$('#hidden1').css("visibility", "visible");
   });
   $('#menu1').mouseleave(function() {
 	$('#menu1').animate({
-		height: "35px"
+		height: "25px"
 	}, 200);
+	$('#hidden1').css("visibility", "hidden");
   });
   $('#menu2').mouseenter(function() {
 	$('#menu2').animate({
-		height: "70px"
+		height: "50px"
 	}, 200);
+	$('#hidden2').css("visibility", "visible");
   });
   $('#menu2').mouseleave(function() {
 	$('#menu2').animate({
-		height: "35px"
+		height: "25px"
 	}, 200);
+	$('#hidden2').css("visibility", "hidden");
   });
   $('#menu3').mouseenter(function() {
 	$('#menu3').animate({
-		height: "70px"
+		height: "50px"
 	}, 200);
+	$('#hidden3').css("visibility", "visible");
   });
   $('#menu3').mouseleave(function() {
 	$('#menu3').animate({
-		height: "35px"
+		height: "25px"
 	}, 200);
+	$('#hidden3').css("visibility", "hidden");
   });
   $('#menu4').mouseenter(function() {
 	$('#menu4').animate({
-		height: "70px"
+		height: "50px"
 	}, 200);
+	$('#hidden4').css("visibility", "visible");
   });
   $('#menu4').mouseleave(function() {
 	$('#menu4').animate({
-		height: "35px"
+		height: "25px"
 	}, 200);
+	$('#hidden4').css("visibility", "hidden");
   });
 
 };
